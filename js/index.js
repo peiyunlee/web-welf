97ee41c30eb727bde4ceb71fc5e6a59f5ae98d8b
PEI-YUN LEE <may1092200258@gmail.com>
2019-10-29 03:07:31 +0800

    scroll

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
-// $(window).scroll(function(){
-//     if($(this).scrollTop()<150){
-//       $('#more').removeClass('notfixed_more');
-//       console.log(true);
-//     }
-//     else{
-//       $('#more').addClass('notfixed_more');
-//       console.log(false);
-//     }
-//   })
 
+// $(document).ready(function () {
+//   $('#hbottom').click(function () {
+//     $('html,body').animate({ 
+//       scrollTop: $('#sec_story').offset().top }, 1000);
+//     });
+// });
 $(document).ready(function () {
-  $('#hbottom').click(function () {
-    $('html,body').animate({ 
-      scrollTop: $('#sec_story').offset().top }, 1000);
-    });
+
+	var controller = new ScrollMagic.Controller();
+
+	new ScrollMagic.Scene({
+		triggerElement: "#sec_story",
+	}).setClassToggle("#nav", "nav_bgcolor1").addIndicators().addTo(controller);
+	
+	new ScrollMagic.Scene({
+		triggerElement: "#sec_play",
+	}).on("add", function (event) {
+		$('#nav').classList.remove("mystyle");
+	});
+
 });
