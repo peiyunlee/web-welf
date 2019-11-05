da1c5570f930060c79dccf62c8b0d574198e9373
Albada <NamelessOAO@gmail.com>
2019-11-05 08:58:13 +0800

    play scroll

--- js/play.js
+++ js/play.js
@@ -5,20 +5,20 @@ $(document).ready(function() {
     new ScrollMagic.Scene({
         triggerElement: "#sec_plays",
         offset: 100,
-    }).setClassToggle(".sw", "upshow").addIndicators().reverse(false).addTo(controller);
+    }).setClassToggle(".sw", "upshow").reverse(false).addTo(controller);
 
     new ScrollMagic.Scene({
         triggerElement: "#sec_plays",
         offset: 100,
-    }).setClassToggle(".wdm", "upshow").addIndicators().reverse(false).addTo(controller);
+    }).setClassToggle(".wdm", "upshow").reverse(false).addTo(controller);
 
     new ScrollMagic.Scene({
         triggerElement: "#sec_plays",
         offset: 100,
-    }).setClassToggle("#iframe", "upshow").addindicator().reverse(false).addTo(controller);
+    }).setClassToggle(".iframe", "upshow_i").reverse(false).addTo(controller);
 
     new ScrollMagic.Scene({
         triggerElement: "#sec_plays",
         offset: 100,
-    }).setClassToggle(".ca", "upshow").addindicator().reverse(false).addTo(controller);
+    }).setClassToggle(".ca", "upshow").reverse(false).addTo(controller);
 });
